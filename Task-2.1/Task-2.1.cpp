8475626e1ba8d038ecba2c52023b7ccbbb65395e
Dmitriy <64133124+DimasPro777@users.noreply.github.com>
2022-09-16 11:10:30 +0300

    Update Task-2.1.cpp

--- Task-2.1/Task-2.1.cpp
+++ Task-2.1/Task-2.1.cpp
@@ -1,14 +1,17 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
+#include <math.h>
 #define pi 3.14159
 
 int main()
 {
     system("chcp 1251");
 
-    double r, R, h, V, l;
+    double r, R, h, l;
+    double x, a;
     cout << "Введите переменные r, R, h: " << std::endl;
     cin >> r >> R >> h;
+
 
     if (r > 0 and R > 0 and h > 0)
     {
@@ -16,5 +19,24 @@ int main()
         l = sqrt(pow(R - r, 2) + pow(h, 2));
         cout << "S = " << pi * (pow(R, 2) + (R + r) * l + pow(r, 2)) << std::endl;
     }
+    else
+    {
+        cout << "Ошибка" << std::endl;   
+    }
+
+
+   
+    cout << "Введите переменные x, a: " << std::endl;
+    cin >> x >> a;
+
+    if (abs(x) < 0)
+    {
+        cout << "w = " << a * log10(abs(x)) << std::endl;
+    }
+    else if (abs(x) >= 0)
+    {
+        cout << "w =" << sqrt(a - pow(x, 2));
+    }
+
     return(0);
 }
