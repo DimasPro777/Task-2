283da6068984ca062a74fff376d1da8bbaa28b38
Дима <@DESKTOP-J3V34PA>
2022-09-16 06:43:14 +0300

    Добавьте файлы проекта.

---
+++ Task-2.1/Task-2.1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+#define pi 3.14159
+
+int main()
+{
+    system("chcp 1251");
+
+    double r, R, h, V, l;
+    cout << "Введите переменные r, R, h: " << std::endl;
+    cin >> r >> R >> h;
+
+    if (r > 0 and R > 0 and h > 0)
+    {
+        cout << "V = " << static_cast<double>(1) / 3 * pi * h * (pow(R, 2) + R * r + pow(r, 2)) << std::endl;
+        l = sqrt(pow(R - r, 2) + pow(h, 2));
+        cout << "S = " << pi * (pow(R, 2) + (R + r) * l + pow(r, 2)) << std::endl;
+    }
+    return(0);
+}
