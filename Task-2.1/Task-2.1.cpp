0fd95888af8fd3d928245e84448aed63cb8728de
Dmitriy <64133124+DimasPro777@users.noreply.github.com>
2022-09-23 10:17:34 +0300

    Update Task-2.1.cpp

--- Task-2.1/Task-2.1.cpp
+++ Task-2.1/Task-2.1.cpp
@@ -8,7 +8,8 @@ int main()
     system("chcp 1251");
 
     double r, R, h, l;
-    double x, a;
+    double x, a, b, y;
+    double n;
     cout << "Введите переменные r, R, h: " << std::endl;
     cin >> r >> R >> h;
 
@@ -21,21 +22,98 @@ int main()
     }
     else
     {
-        cout << "Ошибка" << std::endl;   
+        cout << "Ошибка" << std::endl;
     }
 
 
-   
+
+
+
+
+
+
+
     cout << "Введите переменные x, a: " << std::endl;
     cin >> x >> a;
 
-    if (abs(x) < 0)
+    if (abs(x) < 1 and abs(x) > 0)
     {
-        cout << "w = " << a * log10(abs(x)) << std::endl;
+        cout << "w = " << a * log(abs(x)) << std::endl;
     }
-    else if (abs(x) >= 0)
+    else if (abs(x) >= 1 and a - pow(x, 2) >= 0)
     {
-        cout << "w =" << sqrt(a - pow(x, 2));
+        cout << "w =" << sqrt(a - pow(x, 2)) << std::endl;
+    }
+    else
+    {
+        cout << "Ошибка" << std::endl;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    cout << "Введите переменные b, x, y: " << std::endl;
+    cin >> b >> x >> y;
+    
+    if (b - y > 0 and b - x >= 0)
+    {
+        cout << "z = " << log(b - y) * sqrt(b - x) << std::endl;
+    }
+    else
+    {
+        cout << "Ошибка" << std::endl;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    cout << "Введите переменную n: " << std::endl;
+    cin >> n;
+    int n1 = n;
+    if (n >= 0 and n == n1)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            cout << n + i << std::endl;
+        }
+    }
+    else
+    {
+        cout << "Ошибка" << std::endl;
+    }
+
+
+
+
+
+
+    for (float X = -4; X < 4.5; X += 0.5)
+    {
+        if (x - 1 != 0)
+        {
+            cout << "y = " << (pow(X, 2) - 2 * X + 2) / (X - 1) << " x = " << X << std::endl;
+        }
+        else
+        {
+            cout << "Ошибка"
+        }
     }
 
     return(0);
